1163b0c19eed225bc253d32f1f4630dd78914240
NehaBhatt57 <nehabhatt91757@gmail.com>
2025-09-15 18:33:35 +0530

    changes made

--- app.js
+++ app.js
@@ -5,20 +5,46 @@ const authRoutes = require('./routes/auth');
 const tenantRoutes = require('./routes/tenants');
 const notesRoutes = require('./routes/notes');
 const errorHandler = require('./middleware/errorHandler');
+const { mongo } = require('mongoose');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 
 const app = express();
-(async () => {
-  try {
-    await connectDB();
-    // start listening or export app
+
+let isConnected = false;
+
+async function connectToDatabase() {
+  try{
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log('MongoDB connected');
   } catch (error) {
-    console.error('Failed to connect to MongoDB:', error);
-    process.exit(1);
+    console.error('MongoDB connection error:', error);  
   }
-})();
+  
+}
+
+app.use(async (req, res, next) => {
+  if (!isConnected) {
+    await connectToDatabase();
+  }
+  next();
+});
+
+// (async () => {
+//   try {
+//     await connectDB();
+//     // start listening or export app
+//   } catch (error) {
+//     console.error('Failed to connect to MongoDB:', error);
+//     process.exit(1);
+//   }
+// })();
 
 // connectDB();
 
--- config/db.js
+++ config/db.js
@@ -1,46 +1,46 @@
-const mongoose = require('mongoose');
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-const connectDB = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI, {
-      bufferCommands: false,
-      // Add any other options your MongoDB version requires
-    }).then(mongoose => {
-      console.log('MongoDB connected');
-      return mongoose.connection;
-    });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
-};
-
-module.exports = connectDB;
-
-
-
-
-
 // const mongoose = require('mongoose');
 
+// let cached = global.mongoose;
+
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
+
 // const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI);
-//     console.log('MongoDB connected');
-//   } catch (err) {
-//     console.error(err);
-//     process.exit(1);
+//   if (cached.conn) {
+//     return cached.conn;
 //   }
+
+//   if (!cached.promise) {
+//     cached.promise = mongoose.connect(process.env.MONGO_URI, {
+//       bufferCommands: false,
+//       // Add any other options your MongoDB version requires
+//     }).then(mongoose => {
+//       console.log('MongoDB connected');
+//       return mongoose.connection;
+//     });
+//   }
+
+//   cached.conn = await cached.promise;
+//   return cached.conn;
 // };
 
 // module.exports = connectDB;
+
+
+
+
+
+// // const mongoose = require('mongoose');
+
+// // const connectDB = async () => {
+// //   try {
+// //     await mongoose.connect(process.env.MONGO_URI);
+// //     console.log('MongoDB connected');
+// //   } catch (err) {
+// //     console.error(err);
+// //     process.exit(1);
+// //   }
+// // };
+
+// // module.exports = connectDB;
